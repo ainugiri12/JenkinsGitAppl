bab1338ac6e42ceb0258c9f0016a7a164ef8998d
ainugiri12 <ainugiri@gmail.com>
2022-08-19 17:00:40 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
 public static void main(String giri[]){
  int i = 5;
-      System.out.println(i+" Welcome to Jenkins - Git - GitHub");
+      System.out.println(i+" Welcome to Jenkins - Git - GitHub GIRI");
 }
 }

78eadf177fbde8aa93b8f1a6ad66b34d8df6395e
ainugiri12 <103423770+ainugiri12@users.noreply.github.com>
2022-10-18 15:05:02 +0530

    updated sanple

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
 public static void main(String giri[]){
  int i = 5;
-      System.out.println(i+" Welcome to Jenkins - Git - GitHub GIRI");
+      System.out.println(i+" .NET FSD Capstone Project");
 }
 }

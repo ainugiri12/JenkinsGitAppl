d1bb3f1d8a2f7a1244b7d07cba4970173de7d915
ainugiri12 <103423770+ainugiri12@users.noreply.github.com>
2023-08-04 10:43:46 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
 public static void main(String giri[]){
  int i = 5;
-      System.out.println(i+" .NET FSD Capstone Project");
+      System.out.println(i+" JOSEPHITESt");
 }
 }
